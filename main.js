7dd4fb769bfcb16431b3e4363012473423067124
M3htbD301 <81451074+M3htbD301@users.noreply.github.com>
2022-01-03 15:38:25 -0800

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+leftWristX=0;
+rightWristX=0;
+difference=0;
+
 function setup() {
     video=createCapture(VIDEO);
     video.size(550,500);
@@ -8,14 +12,21 @@ function setup() {
 }
 
 function modelLoaded() {
-    console.log("poseNet is initialiezed")
+    console.log("poseNet is initialized")
 }
 
 function draw() {
     background("#90C3C8");
+    textSize(50);
+    fill("#54426B");
+    text("Change My Color",75, 300);
 }
 function gotPoses(results) {
 if (results.length>0) {
     console.log(results);
+    leftWristX=results[0].pose.leftWrist.x;
+    rightWristX=results[0].pose.rightWrist.x;
+    difference=floor(leftWristX-rightWristX);
+    textSize(difference);
 }
 }
